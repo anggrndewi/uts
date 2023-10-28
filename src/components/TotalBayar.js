775b93633b66911828ccc64341d18bf4c56d3684
Dewi Anggraini <dewi.120140149@student.itera.ac.id>
2023-10-28 16:15:56 +0700

    feat

--- src/components/TotalBayar.js
+++ src/components/TotalBayar.js
@@ -24,22 +24,7 @@ export default class TotalBayar extends Component {
 
     return (
       <>
-        {/* Web */}
         <div className="fixed-bottom d-none d-md-block">
-          <Row>
-            <Col md={{ span: 3, offset: 9 }} className="px-4">
-              <h4>
-                Total Harga : <strong className="float-right mr-2">Rp. {totalBayar}</strong>
-              </h4>
-              <Button variant="primary" block className="mb-2 mt-4 mr-2" size="lg" onClick={() => this.submitTotalBayar(totalBayar)}>
-                <FontAwesomeIcon icon={faShoppingCart} /> <strong>BAYAR</strong>
-              </Button>
-            </Col>
-          </Row>
-        </div>
-
-        {/* Mobile  */}
-        <div className="d-sm-block d-md-none">
           <Row>
             <Col md={{ span: 3, offset: 9 }} className="px-4">
               <h4>
